806c8c4113cc99582f59ed63b2b251d27501fc0e
Buzzology <chrisowens321@gmail.com>
2021-12-30 20:29:53 +1000

    [CHORE] Clustering

--- main/index.js
+++ main/index.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
+const e = require('express');
 
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {
+// Is the file being exceuted in master mode?
+if (cluster.isMaster) {
+    // Cause index.js to be executed again but in child mode.
+    cluster.fork();
+    cluster.fork();
+} else {
+    // I'm a child, ill act like a server and do nothing else.
+    const express = require('express');
+    const app = express();
 
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) {
+
+        }
     }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast');
+    });
+    app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-});
-
-app.listen(3000);
