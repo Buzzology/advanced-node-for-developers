f91d6ba5dc265268045404f9ce8f9bfe09caf4c5
Buzzology <chrisowens321@gmail.com>
2022-01-01 20:34:55 +1000

    [CHORE] Simulating authenticated session with tests

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -34,3 +34,31 @@ test('clicking login starts oauth flow', async () => {
     const url = await page.url();
     expect(url).toMatch(/accounts\.google\.com/);
 })
+
+test.only('When signed in, shows logout button', async () => {
+    // This is the user's id (not google id.)
+    const id = '61ce6ff249529f15c0b9db40';
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id,
+        }
+    };
+
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign(`session=${sessionString}`);
+
+    // Set the cookies for the current page so that we're authenticated.
+    await page.setCookie({name: 'session', value: sessionString });
+    await page.setCookie({name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
+
+    // Ensure that we're logged in by seeing if 'logout' is shown.
+    await page.waitFor('a[href="/auth/logout"]');
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(text).toEqual('Logout');
+})
